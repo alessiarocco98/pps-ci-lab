eff392b5ffc80360a7aa7ac679fe8e34678cd9e7
Marta Spadoni <marta.spadoni2@studio.unibo.it>
2021-04-26 12:47:53 +0200

    Added javadoc

--- src/main/java/Marta.java
+++ src/main/java/Marta.java
@@ -1,6 +1,12 @@
+/**
+ * My class to test ci
+ * **/
 public class Marta {
+    /**
+     * Main method to start my class
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("Hello Marta!");
-        System.out.println("Welcome!");
     }
 }
